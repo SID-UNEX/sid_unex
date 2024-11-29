2df3b14b6db494ab5d1487ba1b9def78d7a100a7
icarovinhas <icaromvinhas@gmail.com>
2024-11-29 15:02:27 -0300

    Atualização

---
+++ script/letras.js
@@ -0,0 +1,141 @@
+function removerAcentos(palavra) {
+    return palavra.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+}
+
+const buttonElements = document.querySelectorAll('button');
+let row = 1;
+let letter = 1;
+let guessedCorrectly = false;
+var res = document.querySelector('div#res')
+
+const wordsForTheWeek = [
+    'Bolsa', 'Lapso', 'Crime', 'Marca', 'Encho', 
+    'Razão', 'Noção', 'Dorme', 'Tempo', 'Juizo', 'Balde', 'Exito', 'Entra', 'Vesgo', 'Etnia', 
+    'Susto', 'Neura', 'Etica', 'Honra', 'Houve', 'Bispo', 'Queda', 'Cesta', 'Pesca', 'Sobre', 
+    'Toque', 'Desde', 'Metro', 'Areia', 'Inibe', 'Omite', 'Prato', 'Heroi', 'Trama', 'Termo', 
+    'Usado', 'Quais', 'Rosna', 'Pisca', 'Outro', 'Inato', 'Cacho', 'Passo', 'Xerem', 'Album', 
+    'Junto', 'Kebab', 'Deixa', 'Quati', 'Untei', 'Puxei', 'Ontem', 'Nariz', 'Graça', 'Anzol', 
+    'Rampa', 'Comer', 'Zelar', 'Justo', 'Legal', 'Ordem', 'Obter', 'Gesto', 'Senso', 'Tenis', 
+    'Jogar', 'Aspas', 'Fugir', 'Graxa', 'Fazer', 'Olhar', 'Nobre', 'Balao', 'Vozes', 'Chuva', 
+    'Gosto', 'Xeque', 'Achar', 'Karma', 'Xampu', 'Yacht', 'Quina', 'Unido', 'Viola', 'Verbo', 
+    'Amado', 'Zarpa', 'Haver', 'Sauna', 'Fisga', 'Jovem', 'Futil', 'Afoga', 'Jeito', 'Poste', 
+    'Quero', 'Nisso', 'Ouvir', 'Zumbi', 'Resto', 'Quilo', 'Daqui', 'Kiwis', 'Lento', 'Besta', 
+    'Roupa', 'Então', 'Troca', 'Zinco', 'Calma', 'Sufle', 'Briga', 'Filma', 'Foice', 'Hotel', 
+    'Ideia', 'Vocês', 'Xingo', 'Igual', 'Jaula', 'Zorra', 'Misto', 'Navio', 'Leite', 'Gosma', 
+    'Globo', 'Tenue', 'Expor', 'Negro', 'Homem', 'Muito', 'Dedao', 'Vento', 'Mexer', 'Sanar', 
+    'Dardo', 'Quase', 'Louco', 'Mania', 'Urina', 'Raspo', 'Moral', 'Veado', 'Hiena', 'Pular', 
+    'Dessa', 'Forma', 'Zanga', 'Icone', 'Urubu', 'Risos', 'Secao', 'Gaita', 'Bucho', 'Itens', 
+    'Livro', 'Creio', 'Impor', 'Ziper', 'Usura', 'Lapis', 'Uniao'
+];
+
+const startDate = new Date('2024-09-03');
+const today = new Date();
+const diffTime = Math.abs(today - startDate);
+const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+const wordForTheDay = wordsForTheWeek[diffDays % wordsForTheWeek.length];
+
+const wordElements = document.querySelectorAll('.word-row');
+let gameOver = false;
+
+buttonElements.forEach((element) => {
+    element.addEventListener('click', function() {
+        keypress(element.attributes["data-key"].value.toUpperCase());
+    });
+});
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    if (key === 'Enter') {
+        keypress('ENTER');
+    } else if (key === 'Backspace') {
+        keypress('DEL');
+    } else if (/^[a-zA-Z]$/.test(key)) {
+        keypress(key.toUpperCase());
+    }
+});
+
+function populateWord(key) {
+    if (letter < 6) {
+        wordElements[row - 1].querySelectorAll('.word')[letter - 1].innerText = key;
+        letter += 1;
+    }
+}
+
+function removerAcentos(palavra) {
+    return palavra.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+}
+
+function checkword() {
+    const letterElements = wordElements[row - 1].querySelectorAll('.word');
+    const wordOfTheDayLower = removerAcentos(wordForTheDay.toLowerCase());
+    const lettersStatus = Array(5).fill(false); // Array para controlar quais letras foram verificadas como verdes
+    let numOfCorrectAlphabets = 0;
+
+    // Primeira passagem: verificar letras verdes
+    letterElements.forEach((element, index) => {
+        const letter = removerAcentos(element.innerText.toLowerCase());
+        if (letter === removerAcentos(wordOfTheDayLower[index])) {
+            numOfCorrectAlphabets += 1;
+            element.classList.add('word-green');
+            lettersStatus[index] = true; // Marca a posição correta como já verificada
+        }
+    });
+
+    // Segunda passagem: verificar letras amarelas
+    letterElements.forEach((element, index) => {
+        const letter = removerAcentos(element.innerText.toLowerCase());
+        if (!lettersStatus[index]) { // Ignorar letras já marcadas como verdes
+            const letterInWordIndex = wordOfTheDayLower.indexOf(letter);
+            if (letterInWordIndex > -1 && !wordElements[row - 1].querySelectorAll('.word')[letterInWordIndex].classList.contains('word-green')) {
+                element.classList.add('word-yellow');
+            } else {
+                element.classList.add('word-grey');
+            }
+        }
+    });
+
+    if (numOfCorrectAlphabets === 5) {
+        gameOver = true;
+        guessedCorrectly = true;
+        res.innerHTML = ('<p><strong>Parabéns 🎉! Você acertou a palavra do dia.</strong></p>');
+    } else if (row === 6) {
+        gameOver = true;
+        res.innerHTML = (`<p>Tenha mais sorte na próxima vez. A palavra era: <strong>${wordForTheDay}</strong></p>`);
+    }
+}
+
+function enterWord() {
+    if (letter < 6) {
+        alert('Tem poucas letras.');
+    } else {
+        checkword();
+        row += 1;
+        letter = 1;
+    }
+}
+
+function deleteLetter() {
+    const letterElements = wordElements[row - 1].querySelectorAll('.word');
+    for (let index = letterElements.length - 1; index >= 0; index--) {
+        const element = letterElements[index];
+        if (element.innerText !== '') {
+            element.innerText = '';
+            letter -= 1;
+            break;
+        }
+    }
+}
+
+function keypress(key) {
+    if (!gameOver) {
+        if (key === 'ENTER') {
+            enterWord();
+        } else if (key === 'DEL') {
+            deleteLetter();
+        } else {
+            populateWord(key);
+        }
+    } else {
+        alert('Fim de jogo! Por favor, tente novamente amanhã e adivinhe uma nova palavra.');
+    }
+}
